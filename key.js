3337391e8fe56de8fe2eec6d7cfa4abae85053cb
GuestAccount <guestAccount@DAVIDs-MacBook-Pro-2.local>
2018-02-10 16:36:36 -0800

    add keys for twitter and spotify -- functions not working

--- key.js
+++ key.js
@@ -1,10 +1,15 @@
-console.log('this is loaded');
-
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-}
+  consumer_key: 'dkoHVSetmwTccXBvgcxDbwnLc',
+  consumer_secret: 'vBKtDluaFufrbqXEAMe5T6YBd6ZB6EORlSCp6yW2U8BMnzTg5E',
+  access_token_key: 'En9eZhfh4beIXkOzVx7dQNN82kf3tkv',
+  access_token_secret: 'JOAWqJuaqOIqPbuYZif5CxwQD5K4CBVAQDm7rtiTrGToH',
+};
 
 module.exports = twitterKeys;
+
+var spotifyKeys = {
+  id: "0bc644846c8f4a32a39f911d14fbc69d",
+  secret: "075bac1367d4474c89d600412851c502"
+};
+
+module.exports = spotifyKeys;
--- liri.js
+++ liri.js
@@ -1,28 +1,107 @@
+var keys = require("./key.js");
+
 var request = require("request");
 
+var Twitter = require('twitter');
+var twitterKeys = keys.twitterKeys;
+
+
+var Spotify = require('node-spotify-api');
+var spotifyKeys = keys.spotifyKeys
+
+var action = process.argv[2];
 var nodeArgs = process.argv;
 
-var movieName = "";
+var value = "";
 
-for (i = 2; i < nodeArgs.length; i++) {
-	if (i > 2 && i < nodeArgs.length) {
-		movieName += "+" + nodeArgs[i];
-	} else {
-		movieName += nodeArgs[i];
-	}
+switch (action) {
+	case "movie-this":
+	movie();
+	break;
+
+	case "my-tweets":
+	twitter();
+	break;
+
+	case "spotify-this-song":
+	spotify();
+	break;
+
+	case "do-what-i-say":
+	spotify(nodeArgs);
+	break;
 }
 
-var queryUrl =  "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=776d35f9";
-
-request(queryUrl, function(error, reponse, body) {
-	if (!error && reponse.statusCode === 200) {
-		console.log("Title: " + JSON.parse(body).Title);
-		console.log("Year: " + JSON.parse(body).Year);
-		console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-		console.log("Rotten Tom Rating: " + JSON.parse(body).Ratings[1].Value);
-		console.log("Country: " + JSON.parse(body).Country);
-		console.log("Language: " + JSON.parse(body).Language);
-		console.log("Plot: " + JSON.parse(body).Plot);
-		console.log("Actors: " + JSON.parse(body).Actors);
+function movie() {
+	for (i = 3; i < nodeArgs.length; i++) {
+		if (i > 3 && i < nodeArgs.length) {
+			value += "+" + nodeArgs[i];
+		} else {
+			value += nodeArgs[i];
+		}
 	}
-});
+
+	if (value === "") {
+		value = "Mr. Nobody"
+	}
+
+	var queryUrl =  "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=776d35f9";
+
+	request(queryUrl, function(error, reponse, body) {
+		if (!error && reponse.statusCode === 200) {
+			console.log("Title: " + JSON.parse(body).Title);
+			console.log("================================================================================");
+			console.log("Year: " + JSON.parse(body).Year);
+			console.log("================================================================================");
+			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+			console.log("================================================================================");
+			console.log("Rotten Tom Rating: " + JSON.parse(body).Ratings[1].Value);
+			console.log("================================================================================");
+			console.log("Country: " + JSON.parse(body).Country);
+			console.log("================================================================================");
+			console.log("Language: " + JSON.parse(body).Language);
+			console.log("================================================================================");
+			console.log("Plot: " + JSON.parse(body).Plot);
+			console.log("================================================================================");
+			console.log("Actors: " + JSON.parse(body).Actors);
+		}
+	});
+}
+
+function twitter() {
+	var client = new Twitter(twitterKeys);
+
+	var params = {screen_name: 'stacysareas', count: 10};
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    console.log(tweets);
+	  }
+	});
+}
+
+function spotify() {
+	var client = new Spotify(spotifyKeys);
+
+	for (i = 3; i < nodeArgs.length; i++) {
+		if (i > 3 && i < nodeArgs.length) {
+			value += "+" + nodeArgs[i];
+		} else {
+			value += nodeArgs[i];
+		}
+	}
+
+	if (song === "") {
+		value = "The sign Ace Of Base";
+	} else {
+		value = song;
+	}
+
+	spotify.search({ type: 'track', query: value }, function(error, data) {
+	  if (error) {
+	    return console.log('Error occurred: ' + error);
+	  }
+	 
+	console.log(data); 
+	});
+}
